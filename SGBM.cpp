1530958fdac40cb6e41bed590e604e0bc2ec55cb
Merritt Jenkins <merritt.jenkins@gmail.com>
2016-12-11 00:39:14 -0500

    Updated SGBM script

--- SGBM.cpp
+++ SGBM.cpp
@@ -94,15 +94,21 @@ int main(int argc, char** argv)
 {
 
     //------------------------------------LOAD IMAGES-----------------------------------------
+
+    //The first two images are the stereo pair. The third image is the one that has moved (we don't know its position)
     std::string img1_filename = "left000007.jpg";
     std::string img2_filename = "right000007.jpg";
     std::string img3_filename = "left000006.jpg";
     std::string disparity_filename = "disparity.jpg";
 
+    //cloud.ply is the point cloud of the plant, features.ply are the 2D features out in 3D
     const char* point_cloud_filename = 0;
+    const char* point_cloud_filename_2 = 0;
 
     point_cloud_filename = "cloud.ply";
+    point_cloud_filename_2 = "features.ply";
 
+    //assign SGBM because it gives superior results
     enum { STEREO_BM=0, STEREO_SGBM=1, STEREO_HH=2, STEREO_VAR=3, STEREO_3WAY=4 };
     int alg = STEREO_SGBM;
 
@@ -115,6 +121,7 @@ int main(int argc, char** argv)
     cout << img1_big.rows << endl;
     cout << img1_big.cols << endl;
 
+    //I was originally downsampling but changed my mind. Leaving the code here in case I change it back.
     Mat img1; Mat img2; Mat img3;
     //pyrDown(img1_big, img1, Size(img1_big.cols/2, img1_big.rows/2));
     //pyrDown(img2_big, img2, Size(img2_big.cols/2, img2_big.rows/2));
@@ -127,11 +134,14 @@ int main(int argc, char** argv)
 
 
     //------------------------------------FIND MATCHES-----------------------------------------
-        clock_t start;
+    
+    //start a timer just for kicks
+    clock_t start;
     double duration;
     start = clock();
 
-    int minHessian = 800;
+    //-- Step 1: Detect keypoints
+    int minHessian = 800; //the hessian appears to affect the number of keypoints. I don't understand what it does.
     SurfFeatureDetector detector(minHessian);
 
     std::vector<KeyPoint> keypoints_1, keypoints_2;
@@ -147,12 +157,9 @@ int main(int argc, char** argv)
     extractor.compute( img1, keypoints_1, descriptors_1 );
     extractor.compute( img3, keypoints_2, descriptors_2 );
 
-    cout << "Im here 1!!" << endl;
-
     //-- Step 3: Matching descriptor vectors using FLANN matcher
     FlannBasedMatcher matcher;
     std::vector< DMatch > matches;
-    cout << "Im here 2!!" << endl;
 
     //Orb features need converted file type
     if(descriptors_1.type()!=CV_32F) {
@@ -160,10 +167,7 @@ int main(int argc, char** argv)
         descriptors_2.convertTo(descriptors_2, CV_32F);
     }
 
-    cout << "Im here 3!!" << endl;
     matcher.match( descriptors_1, descriptors_2, matches );
-
-    //cout << "herrooo" << matches << endl;
 
     double max_dist = 0; double min_dist = 100;
 
@@ -187,7 +191,7 @@ int main(int argc, char** argv)
     std::vector< DMatch > good_matches;
 
     for( int i = 0; i < descriptors_1.rows; i++ )
-    { if( matches[i].distance <= max(2*min_dist, 0.5) )  //this used to be 0.02
+    { if( matches[i].distance <= max(2*min_dist, 0.02) )  //these numbers affect the number of keypoint pairs chosen
         { good_matches.push_back( matches[i]); }
     }
 
@@ -197,32 +201,28 @@ int main(int argc, char** argv)
                good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
                vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
 
+    // end clock, for kicks
     duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
 
-    std::cout<<"printf: "<< duration <<'\n';
+    std::cout<<"Timer: "<< duration <<'\n';
 
     //-- Show detected matches
     namedWindow("Good Matches",WINDOW_NORMAL);
     resizeWindow("Good Matches", 1600,800);
     imshow( "Good Matches", img_matches );
 
-    for( int i = 0; i < (int)good_matches.size(); i++ )
-    { printf( "-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n", i, good_matches[i].queryIdx, good_matches[i].trainIdx ); }
+    cout << "Number of inlier matches: "  << good_matches.size() << endl;
+
+    //for( int i = 0; i < (int)good_matches.size(); i++ )
+    //{ //printf( "-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n", i, good_matches[i].queryIdx, good_matches[i].trainIdx ); 
+    
+    //}
 
     cout << "U,V COORDS OF FIRST MATCHED KEYPOINT: " << keypoints_1[good_matches[0].queryIdx].pt << endl;//good_matches[1].distance << endl;
 
-    Mat matchedKeypoints[2][good_matches.size()];
-
-    //for( int i = 0; i < (int)good_matches.size(); i++ )
-    //{
-    //    cout << keypoints_1[good_matches[i].queryIdx].pt << endl;
-    //}
-
-
     waitKey(0);
 
-    //-----------------------------------------------PROJECT OUT TO 3D-----------------------------------------------
-    
+    //-----------------------------------------------CONVERT TWO IMAGES TO DISPARITY IMAGE-----------------------------------------------
 
     int SADWindowSize;
     bool no_display;
@@ -267,11 +267,11 @@ int main(int argc, char** argv)
 
     int64 t = getTickCount();
 
+    // convert the two images into a disparity image
     sgbm(img1, img2, disp);
     t = getTickCount() - t;
     printf("Time elapsed: %fms\n", t*1000/getTickFrequency());
 
-    //disp = dispp.colRange(numberOfDisparities, img1p.cols);
     if( alg != STEREO_VAR )
         disp.convertTo(disp8, CV_8U, 255/(numberOfDisparities*16.));
     else
@@ -289,48 +289,134 @@ int main(int argc, char** argv)
         printf("writing the disparity file...");
         imwrite(disparity_filename, disp8);
 
-    //Mat mask = disp8 > 79;
-    //cout << mask.rows << endl;
-    //cout << mask.cols << endl;
-    //imshow( "MASK", mask);
-    //cout << mask << endl;
-    //Mat outcolors = img1(mask);
-
+    // M1/M2 are camera intrinsics (known) and D1/D2 are camera distortion parameters (known)
     Mat M1, D1, M2, D2;
     M1 = (Mat_<double>(3,3) << 2421.988247695817, 0.0, 1689.668741757609, 0.0, 2424.953969600827, 1372.029058638022, 0.0, 0.0, 1.0);
     M2 = (Mat_<double>(3,3) << 2402.822932836092, 0.0, 1715.977908625392, 0.0, 2405.531742202912, 1356.326619144245, 0.0, 0.0, 1.0);
     D1 = (Mat_<double>(5,1) << -0.001013934596658032, 0.00355911874186913, -0.0006557984914098192, -0.0009806454600919795, 0.0);
     D2 = (Mat_<double>(5,1) << -0.00596545966775133, 0.0056628943340289, -0.0008602296090100512, 6.54885403584621e-05, 0.0);
 
-    //[2000,0,0; 0, 2000,0; 0, 0, 1] >> M1;
-
+    // R is the rotation between the two lenses (pretty much identity)
+    // T is the translation between the two lenses (~10cm, but that gives weird world units...)
     Mat R, T, R1, P1_rec, R2, P2_rec;
     Mat Q;
-    R = (Mat_<double>(3,3) << 0.9999698296761699, 0.000573862809423943, -0.00774663919954301, -0.000574843594088605, 0.9999998270419937, -0.0001243817708070411, 0.007746566481627299, 0.0001288311240883246, 0.9999699866047417); //1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0);
-    T = (Mat_<double>(3,1) << 0.0, 2.0, 0.0); //-219.5597042086226, 0.0, 0.0);
+    //R = (Mat_<double>(3,3) << 0.9999698296761699, 0.000573862809423943, -0.00774663919954301, -0.000574843594088605, 0.9999998270419937, -0.0001243817708070411, 0.007746566481627299, 0.0001288311240883246, 0.9999699866047417); //1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0);
+    R = (Mat_<double>(3,3) << 1,0,0,0,1,0,0,0,1);
+    T = (Mat_<double>(3,1) << 100.0, 0.0, 0.0); //-219.5597042086226, 0.0, 0.0);
 
-    
+    //This is just to get a Q matrix
     stereoRectify( M1, D1, M2, D2, img_size, R, T, R1, R2, P1_rec, P2_rec, Q, CALIB_ZERO_DISPARITY, -1, img_size, &roi1, &roi2 );  
 
-    cout << Q << endl;  
+    cout << "\n" << Q << endl; 
 
-    //if(point_cloud_filename)
-    //{
-        printf("storing the point cloud...");
-        fflush(stdout);
-        Mat xyz;
-        reprojectImageTo3D(disp, xyz, Q, true);
-        //cvtColor(img1, COLOR_BGR2RGB)
+    //float cx = ;
+    //float cy = ;
+    //float f = 2421;
+    //float T = 100; 
 
-        saveXYZ(point_cloud_filename, xyz, img1);
-        printf("\n");
-    //}
+    //Q = (Mat_<double>(4,4) << 1,0,0,-cx,0,1,0,-cy,0,0,0,f,0,0,-1/T,0);
 
-//-------------------------------------------------PROJECT 3D POINTS TO IMAGE 6----------------------------------------------
+    //---------------------------------------------------WRITE 3D CLOUD TO FILE--------------------------------------------    
+
+    printf("storing the point cloud...");
+    fflush(stdout);
+    Mat xyz;
+    reprojectImageTo3D(disp, xyz, Q, true);
+    //cvtColor(img1, COLOR_BGR2RGB)
+
+    saveXYZ(point_cloud_filename, xyz, img1);
+    printf("\n");
 
 
+    //-------------------------------------------------FIND FEATURE POINTS IN 3D----------------------------------------------
+
+    FILE* fp = fopen(point_cloud_filename_2, "wt");
+    std::ostringstream stream;
+
+    float u;
+    float v;
+
+    vector<Point3f> list_points3d;
+    vector<Point2f> list_points2d;
+    vector<KeyPoint> keypoints_im2;
+
+    //This loops through all of the matched feature points and finds the image coordinate in 3D. If the z-coordinate is less than 3D (ie if the block matching found a match),
+    //then the 3d points are saved to a vector and the 2D feature points in the second image (image 6)
+    for( int i = 0; i < (int)good_matches.size(); i++ )
+    {
+        //cout << keypoints_1[good_matches[i].queryIdx].pt << endl;
+        u = keypoints_1[good_matches[i].queryIdx].pt.x;
+        v = keypoints_1[good_matches[i].queryIdx].pt.y;
+        Vec3f point = xyz.at<Vec3f>(v, u);
+        if (point[2]<10000)
+        {
+            //fprintf(fp, "%f %f %f\n", point[0], point[1], point[2]);
+            list_points3d.push_back(Point3f(point[0],point[1],point[2]));
+            list_points2d.push_back(Point2f(keypoints_2[good_matches[i].trainIdx].pt.y, keypoints_2[good_matches[i].trainIdx].pt.x));
+            keypoints_im2.push_back(KeyPoint(keypoints_2[good_matches[i].trainIdx]));
+        }
+    }
 
 
+//----------------------------------------------------------SOLVE PNP------------------------------------------------------
+
+    bool flags = 0;
+    Mat distCoeffs = Mat::zeros(4, 1, CV_64FC1);
+    Mat rvec = Mat::zeros(3, 1, CV_64FC1);
+    Mat tvec = Mat::zeros(3, 1, CV_64FC1);
+
+    Mat R_matrix = Mat::zeros(3, 3, CV_64FC1);
+    Mat t_matrix = Mat::zeros(3, 1, CV_64FC1);
+
+    bool useExtrinsicGuess = false;
+    int iterationsCount=2000;
+    float reprojectionError=50.0;
+    double confidence=0.99;
+    //InputOutputArray inliers=noArray();
+    Mat inliers;
+
+    solvePnP( list_points3d, list_points2d, M1, distCoeffs, rvec, tvec, useExtrinsicGuess, flags);
+
+    //solvePnPRansac( list_points3d, list_points2d, M1, distCoeffs, rvec, tvec, useExtrinsicGuess, iterationsCount, reprojectionError, confidence, inliers, flags);
+    
+    Rodrigues(rvec,R_matrix);
+    t_matrix = tvec;
+
+    cout << "R_matrix: " << R_matrix << endl;
+    cout << "t_matrix: " << t_matrix << endl;
+    cout << "inliers: " << inliers << endl;
+
+
+    Mat jacobian;
+    double aspectRatio = 0;
+    vector<Point2f> imagePoints; 
+    projectPoints (list_points3d,rvec,tvec,M1,distCoeffs,imagePoints,jacobian,aspectRatio);   
+
+    //cout << imagePoints << endl;
+    cout << "Number of imagePoints: " << imagePoints.size() << endl;
+
+    vector<KeyPoint> keypoints_projected;
+    for( size_t i = 0; i < imagePoints.size(); i++ ) {
+        keypoints_projected.push_back(KeyPoint(imagePoints[i], 1.f));
+    }
+
+    cout << "Success!" << endl;
+
+    Mat im_keypoints;
+    drawKeypoints(img3, keypoints_im2, im_keypoints, Scalar::all(-1),4);
+
+    Mat im_comparekeypoints;
+    drawKeypoints(im_keypoints, keypoints_projected, im_comparekeypoints, Scalar::all(-1),4);
+
+    namedWindow("Matches",WINDOW_NORMAL);
+    resizeWindow("Matches", 800,800);
+    imshow("Matches", im_keypoints);
+
+    namedWindow("Matches2",WINDOW_NORMAL);
+    resizeWindow("Matches2", 800,800);
+    imshow("Matches2", im_comparekeypoints);
+
+    waitKey(0);
 
     return 0;
 }
